9109346cfb61ffa2624a9969b73fdaa3897b2a26
David <synlike@gmail.com>
2023-09-18 18:18:03 +0200

    Small modif

--- Assets/Scripts/Shoot/Shootable.cs
+++ Assets/Scripts/Shoot/Shootable.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 
 public abstract class Shootable : MonoBehaviour
 {
-    public static event Action<Shootable, RaycastHit> OnTookShoot = (shootable, hit) => { };
-
     public virtual void TakeShot(RaycastHit hit)
     {
-        OnTookShoot(this, hit);
     }
 }
